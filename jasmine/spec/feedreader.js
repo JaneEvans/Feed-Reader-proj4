01233b0ca6603c1c58aaa73d37b2fc368bbcdf1d
JaneEvans <jinjin.ge@utexas.edu>
2018-10-08 22:40:16 -0700

    test menu toggle on/off functionality

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,17 +55,15 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
+        const body = $('body')[0];
 
-    
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
         it('is hidden', function() {
-            const body = $('body')[0];
             expect(body).toHaveClass('menu-hidden');
-            // console.log(body);
         });
 
          /* TODO: Write a test that ensures the menu changes
@@ -73,6 +71,16 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it('toggles on and off', function() {
+            const menu = $('.menu-icon-link');
+
+            menu.click();
+            expect(body).not.toHaveClass('menu-hidden');
+
+            menu.click();
+            expect(body).toHaveClass('menu-hidden');
+
+         });
     });
 
 
